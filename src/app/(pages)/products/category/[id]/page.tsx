3aaf884c98e7ab703ae3091be11efee58bd16d01
KB <karol.barkowski@gmail.com>
2024-07-12 15:01:09 +0200

    products in category page fix

--- src/app/(pages)/products/category/[id]/page.tsx
+++ src/app/(pages)/products/category/[id]/page.tsx
@@ -1,7 +1,7 @@
-import payload from 'payload'
 import { Container } from '../../../../_components/Container'
 import { CategoryNavigation } from '../../components/CategoryNavigation'
 import { categoryFetchAll } from '../../../../_api/productCategories'
+import { ProductsList } from '../../components/ProductList'
 
 // Force this page to be dynamic so that Next.js does not cache it
 // See the note in '../../../[slug]/page.tsx' about this
@@ -12,7 +12,7 @@ export default async function Product({ params: { id } }) {
     <Container className="grid grid-cols-4">
       <CategoryNavigation id={id} />
       <div className="col-span-3">
-        {/* <ProductsList endpointName={EndpointNames.Products.InCategory(id)} /> */}
+        <ProductsList listType="incategory" categoryId={id} />
       </div>
     </Container>
   )
